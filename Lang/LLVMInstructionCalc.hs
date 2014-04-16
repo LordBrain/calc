0d5005990009da8d125d8ec0505f3ea481a09bc5
James Crayne <jim.crayne@gmail.com>
2014-04-16 02:36:00 -0400

    cleaning.. refactoring

--- Lang/LLVMInstructionCalc.hs
+++ Lang/LLVMInstructionCalc.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE PatternGuards #-}
+{-# LANGUAGE ViewPatterns #-}
 module Lang.LLVMInstructionCalc where
 
 import Lang.AbsCalc
 import Lang.ErrM
+import qualified Lang.TypifyCalc as TY
+import Lang.TypifyCalc (getTyp)
+
 import LLVM.General.AST
 import LLVM.General.AST.Instruction
 import LLVM.General.Module
@@ -31,8 +35,14 @@ type Result = Err [Named Instruction]
 updateNames :: Name -> Word -> [Named Instruction] -> [Named Instruction] 
 updateNames rName unNameOffset ins = update' ins 
    where
-       update' [ _ := instruction ] = [ rName := updateOperands instruction ] -- singleton list
+       update' [ _ := instruction ] = [ appropriate rName := updateOperands instruction ] -- singleton list
        update' ((x := i):is)  = (updateName x := updateOperands i):update' is
+
+       -- approprate final instruction name
+       -- if rName is an UnName then ignore it
+       appropriate x | UnName _ <- rName = updateName x
+       appropriate x@(UnName n) = updateName x -- UnName (n+unNameOffset)
+       appropriate _ = rName
 
        updateName :: Name -> Name
        updateName x@(Name s) | x <- rName = Name (s ++ "0")
@@ -56,18 +66,81 @@ transIdent :: Ident -> Result
 transIdent x = case x of
   Ident str  -> failure x
 
+data BinOP = BAdd | BSub | BMul | BDiv | BNone deriving (Eq,Ord,Show)
+
+getBinOP x = case x of
+  EPos pos n typ exp  -> getBinOP exp
+  EAs typ exp  -> getBinOP exp
+  EAdd {}  -> BAdd
+  ESub {}  -> BSub
+  EMul {}  -> BMul
+  EDiv {}  -> BDiv
+  _        -> BNone
+
+allbutlast xs@(length -> n) = take (n-1) xs
+
+getBinOpInstruction :: Exp -> Operand -> Operand -> InstructionMetadata -> Instruction
+getBinOpInstruction x = case (getTyp x, getBinOP x) of
+    (TI32,BAdd) -> Add False False
+    (TFlo,BAdd) -> FAdd
+    (TI32,BSub) -> Sub False False
+    (TFlo,BSub) -> FSub
+    (TI32,BMul) -> Mul False False
+    (TFlo,BMul) -> FMul
+    (_,   BDiv) -> FDiv
+    -- Using Add 0 for storing Terminals as instructions
+    (TI32,BNone) -> Add False False
+    (TFlo,BNone) -> FAdd
+    
+zeroConstant TI32 = ConstantOperand (C.Int 32 0)
+zeroConstant  _   = ConstantOperand (C.Float $ Fl.Double 0)
+
+getTerminal :: Exp -> Err Operand
+getTerminal (EPos _ _ typ x) = getTerminal (EAs typ x)
+getTerminal (EAs typ x) = getTerminal x
+getTerminal (EVar (Ident x)) = return $ LocalReference (Name x)
+getTerminal (EInt n) = return $ ConstantOperand (C.Int 32 n)
+getTerminal (EFlo n) = return $ ConstantOperand (C.Float $ Fl.Double n)
+getTerminal x = error ("Expected terminal, syntax:" ++ show x)
+
+getFirstParam :: Exp -> Err Exp
+getFirstParam (EPos _ _ typ x) = getFirstParam (EAs typ x)
+getFirstParam (EAs typ x) = getFirstParam x
+getFirstParam x 
+ | EAdd exp _ <- x = return exp
+ | ESub exp _ <- x = return exp
+ | EMul exp _ <- x = return exp
+ | EDiv exp _ <- x = return exp
+getFirstParam x = error ("Could not get first parameter, syntax: " ++ show x)
+
+getSecondParam (EPos _ _ typ x) = getSecondParam (EAs typ x)
+getSecondParam (EAs typ x) = getSecondParam x
+getSecondParam x 
+ | EAdd _ exp  <- x = return exp
+ | ESub _ exp  <- x = return exp
+ | EMul _ exp  <- x = return exp
+ | EDiv _ exp  <- x = return exp
+getSecondParam x = error ("Could not get second parameter, syntax: " ++ show x)
 
 transExp :: Exp -> Result
-transExp x = case x of
-  EPos pos n typ exp  -> failure x
-  EAs typ exp  -> failure x
-  EAdd exp1 exp2  -> failure x
-  ESub exp1 exp2  -> failure x
-  EMul exp1 exp2  -> failure x
-  EDiv exp1 exp2  -> failure x
-  EInt n  -> failure x
-  EFlo d  -> failure x
-  EVar id  -> failure x
+transExp x = case (getTyp x, getBinOP x) of
+    (typ, BNone) -> getTerminal x >>= \op -> return [UnName 0 := getBinOpInstruction x op (zeroConstant typ) [] ]
+    (typ, _    ) -> do
+        exp1 <- getFirstParam x
+        exp2 <- getSecondParam x
+        xs <- transExp exp1
+        ys <- fmap (updateNames (UnName 0) (fromIntegral $ length xs)) (transExp exp2)
+        let x1@(UnName n1) := i1 = last xs
+            y1@(UnName n2) := i2 = last ys
+            n3 = 1 + max n1 n2
+            newname = UnName n3
+            (xs',op0') = if getBinOP exp1 == BNone then ([], getTerminal exp1)
+                                                   else (xs, return $ LocalReference x1)
+            (ys',op1') = if getBinOP exp2 == BNone then ([],  getTerminal exp2)
+                                                   else (ys, return $ LocalReference y1)
+        op0 <- op0'
+        op1 <- op1'
+        return $ xs ++ ys ++ [newname := getBinOpInstruction x op0 op1 []]
 
 
 transTyp :: Typ -> Result
